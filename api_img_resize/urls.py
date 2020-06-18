d2438e5f0affdd251c5306652e0f0dc1b69e8497
Kostya Vinogradov <kostya.06.05.1995@gmail.com>
2020-06-18 12:43:47 +0300

    Переименовано url api для прода.

--- api_img_resize/urls.py
+++ api_img_resize/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'api_resize_task'
 urlpatterns = [
-    path('1/', views.TaskCreateView.as_view()),
-    path('2/<str:task_id>/', views.TaskCheckView.as_view()),
-    path('3/<str:task_id>/', views.TaskDeleteView.as_view()),
+    path('task/create/', views.TaskCreateView.as_view()),
+    path('task/check/<str:task_id>/', views.TaskCheckView.as_view()),
+    path('task/delete/<str:task_id>/', views.TaskDeleteView.as_view()),
 ]
